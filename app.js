7b9b4007897f4207765d3c78c5d5906e8d2b9c34
Violet Stewart <vkazoo2@gmail.com>
2021-10-08 13:30:16 -0700

    button does generate random number between 1-100 but I want to try and make button generate numbers up to infinity

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 // import functions and grab DOM elements
+const button = document.getElementById('generate');
+const result = document.getElementById('random-num');
 
-// initialize global state
+//function getRandomIntInclusive(min, max) {
+//min = Math.ceil(min);
+//max = Math.floor(max);
+//return Math.floor(Math.random() * (max - min + 1) + min);
+//}; didnt work, not sure why
+//console.log(getRandomIntInclusive);
 
-// set event listeners 
-// get user input
-// use user input to update state 
-// update DOM to reflect the new state
+const randomNumber = Math.floor(Math.random() * 100 + 1);
+console.log(randomNumber);
 
+//I know the Build is asking us to generate a random number inclusive but I cant figure that out so I am going to move on and make my button work!
+
+button.addEventListener('click', () => {
+    //console.log('button was clicked');
+    result.textContent = randomNumber;
+});
